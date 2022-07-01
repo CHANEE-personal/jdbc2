2334777c5517ed852788a908762faf6809f76c48
CHO CHAN HEE <cksgml4424@gmail.com>
2022-07-01 14:18:57 +0900

    init

--- src/main/java/hello/itemservice/config/JdbcTemplateV1Config.java
+++ src/main/java/hello/itemservice/config/JdbcTemplateV1Config.java
@@ -1,6 +1,11 @@
 package hello.itemservice.config;
 
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.jdbctemplate.JdbcTemplateImteRepositoryV1;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
@@ -11,5 +16,13 @@ public class JdbcTemplateV1Config {
 
     private final DataSource dataSource;
 
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
 
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JdbcTemplateImteRepositoryV1(dataSource);
+    }
 }
