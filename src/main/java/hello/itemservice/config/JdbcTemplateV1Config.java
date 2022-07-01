072dd2bc15ccc4f1883d3faa6857d0e00edb254e
ChoChanHui <chanhee.cho@ppbstudios.com>
2022-07-01 13:10:10 +0900

    init

---
+++ src/main/java/hello/itemservice/config/JdbcTemplateV1Config.java
@@ -0,0 +1,15 @@
+package hello.itemservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class JdbcTemplateV1Config {
+
+    private final DataSource dataSource;
+
+
+}
